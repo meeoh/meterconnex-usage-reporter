616a0cb211a878b71c04bf331679a210f660ae43
Shameel Abdullah <shameel.abdullah@gmail.com>
2019-10-20 18:45:12 -0400

    typo in run scripts

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build -t meterconnex-repporter .
+docker build -t meterconnex-reporter .
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker run -i --init --rm --cap-add=SYS_ADMIN \
-   --name puppeteer-chrome meterconnex-repporter \
+   --name puppeteer-chrome meterconnex-reporter \
    node -e "`cat index.js`"
